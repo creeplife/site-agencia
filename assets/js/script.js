bad0ddb65438f637f5cc956608c03d1fca4b656a
creeplife <69985739+creeplife@users.noreply.github.com>
2026-08-25 23:20:32 +0000

    25/08

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,30 +58,28 @@ loop();
 // #region swiper 
 
 
-const swiperEl = document.querySelector('swiper-container');
-Object.assign(swiperEl, {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    clickable: true,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-  },
-});
-swiperEl.initialize();
+const swiperEl = document.querySelector('.mySwiper');
 
+Object.assign(swiperEl, {
+    slidesPerView: 3,
+    centeredSlides: true,
+
+
+    spaceBetween: 30,
+
+    loop: true,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+                pauseOnMouseEnter: true
+
+    },
+
+    mousewheel: true
+});
+
+swiperEl.initialize();
 
 
 // #endregion 
